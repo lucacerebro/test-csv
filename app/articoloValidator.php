c87be8d3f550080336af5715bef549315f75ecaf
Luca Verrino <luca.verrino@beexel.it>
2016-10-07 12:51:23 +0200

    function array_Column per php 5.4

--- app/articoloValidator.php
+++ app/articoloValidator.php
@@ -17,6 +17,7 @@ use App\Articolo;
 use App\Csv_importati;
 use Illuminate\Validation\Factory as ValidationFactory;
 use DB;
+use App\a_column;
 
 class articoloValidator {
     private $validator;
@@ -56,6 +57,7 @@ class articoloValidator {
         $this->validator = $validator;
         $this->articolob = new Articolo;
         $this->csv_import= new Csv_importati;
+        $this->a_column= new a_column;
     }
     
     public function validate($csv_file_path){
@@ -67,8 +69,16 @@ class articoloValidator {
         $scont= Articolo_categoria_sconto::all(['id','codice']);
         $unitamisura = Unita_misura::all(['id','codice']);
         $artcs=  Articolo::all(['id','codice']);
-        $art_array=$artcs->toArray();
-        
+        //$art_array=$artcs->toArray();
+   /*     $artcs_id= Articolo::all(['id']);
+        $artcs_cod= Articolo::all(['codice']);
+        $id_array=$artcs_id->toArray();
+        $cod_array=$artcs_cod->toArray();
+    * 
+    */
+       $artcs_array=$artcs->toArray();
+        //foreach ($artcs_array as $cd) echo implode($cd).'<br>';
+        //echo '<br> -> '.$artcs_array[0]['id']['codice'].'<br>';
         echo memory_get_usage().'<br>';
 
         ini_set("auto_detect_line_endings", true);
@@ -99,30 +109,33 @@ class articoloValidator {
              
             $cod=$data_row['codice'];
             echo $cod.'<br>';
+           $column_cod=$this->a_column->ar_column($artcs_array,'codice');
+          //  echo implode($var,';').'<br>';
           //  $arts= $artcs->where('codice', $cod)->first();
             //$arts= DB::table('articolo')->where('codice',$cod)->first();
             //$arts= Articolo::where('codice',$cod)->first();
          //  $arts= $this->articolob->where('codice',$cod)->first();
-            $key=  array_search($cod,  array_column($art_array,'codice'));
-            //echo 'Key : '.$key.'<br>';
-            //echo $art_array[$key]['id'];
+           // $key=  array_search($cod,  array_column($art_array,'codice'));
+            $key=  array_search($cod,  $column_cod);
+          
+            echo 'Key : '.$key.'<br>';
             if(!empty($key))
             {
                echo 'YES<br>';
 
                 //$id=$arts['id'];
-                $id=$art_array[$key]['id'];
-                    //CONVERTE IL CODICE IVA IN ID DELLA TABELLA IVA
-                    $data_row['iva']=$codici_iva->where('codice',$data_row['iva'])->first()->id;
-                   //  $data_row['iva']= $this->articolob->get_iva_id($data_row['iva']);  
-                   $data_row['aspetto_bene']= $aspettobene->where('codice',$data_row['aspetto_bene'])->first()->id;
-                   //$data_row['aspetto_bene']=  $this->articolob->get_aspetto_id($data_row['aspetto_bene']);
-                    $data_row['unita_misura']= $unitamisura->where('codice',$data_row['unita_misura'])->first()->id;
-                   //$data_row['unita_misura']=  $this->articolob->get_misura_id($data_row['unita_misura']);
-                   $data_row['sconto']= $scont->where('codice',$data_row['sconto'])->first()->id;
-                   //$data_row['sconto']= $this->articolob->get_cat_sconto_id($data_row['sconto']);  
-                   $data_row['provv']= $provv->where('codice',$data_row['provv'])->first()->id;
-                   //$data_row['provv']= $this->articolob->get_cat_provv_id($data_row['provv']);  
+                $id=$artcs_array[$key]['id'];
+                //CONVERTE IL CODICE IVA IN ID DELLA TABELLA IVA
+                $data_row['iva']=$codici_iva->where('codice',$data_row['iva'])->first()->id;
+                //  $data_row['iva']= $this->articolob->get_iva_id($data_row['iva']);  
+                $data_row['aspetto_bene']= $aspettobene->where('codice',$data_row['aspetto_bene'])->first()->id;
+                //$data_row['aspetto_bene']=  $this->articolob->get_aspetto_id($data_row['aspetto_bene']);
+                $data_row['unita_misura']= $unitamisura->where('codice',$data_row['unita_misura'])->first()->id;
+                //$data_row['unita_misura']=  $this->articolob->get_misura_id($data_row['unita_misura']);
+                $data_row['sconto']= $scont->where('codice',$data_row['sconto'])->first()->id;
+                //$data_row['sconto']= $this->articolob->get_cat_sconto_id($data_row['sconto']);  
+                $data_row['provv']= $provv->where('codice',$data_row['provv'])->first()->id;
+                //$data_row['provv']= $this->articolob->get_cat_provv_id($data_row['provv']);  
                
                     array_unshift($data_row, $id);
                     //$articoli_validati[]=$data_row;
