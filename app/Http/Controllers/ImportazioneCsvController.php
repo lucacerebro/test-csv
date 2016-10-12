63102283167ef9ecaacff793f875798fb2dc1cae
Luca Verrino <luca.verrino@beexel.it>
2016-10-12 12:33:01 +0200

    recommit

--- app/Http/Controllers/ImportazioneCsvController.php
+++ app/Http/Controllers/ImportazioneCsvController.php
@@ -62,9 +62,8 @@ class ImportazioneCsvController extends Controller
             return view('showone',['art' => $art]);
             }
     public function downloadExcel($type){
-	echo 'in function downloadexcel';
         ini_set('max_execution_time', 600);
-        $data = Articolo::get()->toArray();	
+        $data = Articolo::get()->toArray();
 	return Excel::create('articoli_presenti', function($excel) use ($data) {
 	$excel->sheet('mySheet', function($sheet) use ($data)
             {
