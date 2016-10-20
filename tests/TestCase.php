268b90df71bbae266a0690c676d3c3edaa29052a
lucacerebro <lucacerebro@gmail.com>
2016-10-20 10:36:27 +0200

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	/**
 	 * Creates the application.
 	 *
+	 *
 	 * @return \Illuminate\Foundation\Application
 	 */
 	public function createApplication()
