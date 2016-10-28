10ac26f77fe6f49807b7e03abaedc222fef876e6
Luca Verrino <luca.verrino@beexel.it>
2016-10-28 14:38:13 +0200

    no message

--- app/articoloValidator.php
+++ app/articoloValidator.php
@@ -283,10 +283,12 @@ class articoloValidator {
         echo $path.'<br>';
         $con = mysqli_init();
         $con->options(MYSQLI_OPT_LOCAL_INFILE, true);
-        $path='/var/lib/mysql/bianchi16.csv';
+//        $path='/var/lib/mysql/bianchi16.csv';
         //ini_set('mysql.allow_local_infile', 1);
         $query = sprintf("LOAD DATA LOCAL INFILE '%s' REPLACE INTO TABLE  articolo FIELDS TERMINATED BY ';' LINES TERMINATED BY '\\n' ", addslashes($path),$name_tab);
         echo $query;
+
+           //     $f=  fopen(__DIR__.'/../storage/imports/'.$path, 'r');
         DB::connection()->getPdo()->exec($query);
         return 1;
     }
